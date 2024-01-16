bdd26fd764cbf035d36c06e925a9e8e91cdab808
Debra Winter <deb@debwinter.com>
2024-01-16 14:22:00 -0500

    added string interpolation

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -22,7 +22,7 @@
 
 
             var answer = 4;
-            var response = (answer < 9) ? " is less than nine" : " is greater than or equal to nine";
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
 
         }
     }
